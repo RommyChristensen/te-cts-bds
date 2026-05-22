bcd351b9d287249b2989ef1730c2ce3a3cd75be1
RommyChristensen <rommycy00@gmail.com>
2026-05-22 10:42:34 +0700

    fix bugs

--- migrations/add_game_file_name.sql
+++ migrations/add_game_file_name.sql
@@ -1,5 +1,11 @@
--- Add file_name column to games table
-ALTER TABLE games ADD COLUMN file_name VARCHAR(255);
-
--- Add comment to describe the column
-COMMENT ON COLUMN games.file_name IS 'JavaScript file name for the game implementation';
+-- Add file_name column to games table if it doesn't exist
+DO $$ 
+BEGIN 
+  IF NOT EXISTS (
+    SELECT 1 FROM information_schema.columns 
+    WHERE table_name = 'games' AND column_name = 'file_name'
+  ) THEN 
+    ALTER TABLE games ADD COLUMN file_name VARCHAR(255);
+    COMMENT ON COLUMN games.file_name IS 'JavaScript file name for the game implementation';
+  END IF;
+END $$;
